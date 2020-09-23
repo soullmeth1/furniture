30d93ecb82a238e9f493a188c87256b05dc3ed73
soullmeth1 <soullmeth1@gmail.com>
2020-09-23 18:33:41 +0700

    Updates

---
+++ precache-manifest.f60753ae64d850e5f82740058f3d0cb1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8240065a64dacadca7f89139d4bb4b6",
+    "url": "/furniture/index.html"
+  },
+  {
+    "revision": "12089b51db932c919a8b",
+    "url": "/furniture/static/css/main.3f989b68.chunk.css"
+  },
+  {
+    "revision": "b99ee47e29d5eedda732",
+    "url": "/furniture/static/js/2.056e29df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/furniture/static/js/2.056e29df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12089b51db932c919a8b",
+    "url": "/furniture/static/js/main.d4f8817a.chunk.js"
+  },
+  {
+    "revision": "18cc90b3d8b567cdd2e1",
+    "url": "/furniture/static/js/runtime-main.9df13dd7.js"
+  },
+  {
+    "revision": "c8106259208c43ee8cfbbafbcd3f0ec4",
+    "url": "/furniture/static/media/banner.c8106259.jpg"
+  },
+  {
+    "revision": "38901fc1a7aebfb22e564a2eedd01d44",
+    "url": "/furniture/static/media/chair.38901fc1.png"
+  },
+  {
+    "revision": "b1abd84174cdd708c9626c64bd01cc23",
+    "url": "/furniture/static/media/chair1.b1abd841.png"
+  },
+  {
+    "revision": "d1e00e169201d4a7356f34fb602e62ed",
+    "url": "/furniture/static/media/lamp.d1e00e16.png"
+  },
+  {
+    "revision": "d555cb0fbafde6afb4cbf192e7ed3032",
+    "url": "/furniture/static/media/lamp1.d555cb0f.png"
+  },
+  {
+    "revision": "35652cc0efa05bd622b7bfb00dd2748b",
+    "url": "/furniture/static/media/lamp2.35652cc0.png"
+  },
+  {
+    "revision": "c0f5ca99801b8da84bcad953e322ca22",
+    "url": "/furniture/static/media/set.c0f5ca99.png"
+  }
+]);
